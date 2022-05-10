f1ba86d565b5160bcff82757ade96cb1fabd93f5
Anh Truong <70963346+dinhanhtruong@users.noreply.github.com>
2022-05-10 17:35:28 -0400

    feat: converted listings page claimer ID to name

--- frontend/src/SharedComponents/Listing.tsx
+++ frontend/src/SharedComponents/Listing.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {getImageSrc} from "./UtilFunctions";
 import "../SharedComponents/Listing.css"
+import {getDatabase, onValue, ref} from "firebase/database";
+import {UserData} from "../Profile/ProfilePage";
 
 // Type for the JSON data of a single listing = map from string -> string (field name -> field value)
 export type ListingData = {
@@ -24,12 +26,31 @@ interface ListingProps {
 
 function Listing(props: ListingProps) {
     const [img, setImg] = useState("");
-
+    const [userName, setUserName] = useState<string>("");
+    const [userEmail, setUserEmail] = useState<string>("");
+    const [userPhone, setUserPhone] = useState<string>("");
 
     const loadImg = async (relativeImgURL: string) => {
         // resolve promise by only updating img AFTER the promise is returned from getImageSrc
         const res = await getImageSrc(relativeImgURL)
         setImg(res)
+    }
+
+    // access profile info of claimer if it exists
+    const getUserClaim = () => {
+        // get database
+        const db = getDatabase()
+        const userRef = ref(db, 'users/' + props.data.user_id);
+        // Get info of the claimer of this listing
+        onValue(userRef, (snapshot) => {
+                const data : UserData = snapshot.val();
+                setUserName(data.name)
+                setUserEmail(data.email)
+                setUserPhone(data.phone)
+            },
+            {
+                onlyOnce: true
+            })
     }
 
     // load image once upon initial rendering of listing
@@ -38,12 +59,23 @@ function Listing(props: ListingProps) {
         loadImg(`${props.listingName}/img1`)
     }, [props.listingName])
 
+    // get detail of claimers (if any)
+    useEffect(() => {
+        if(props.data.user_id != "") {
+            getUserClaim()
+        }
+        }, [props.data.user_id]);
+
+
     /* Event handlers */
     const onClickAccept = (e: React.MouseEvent) => {
         e.stopPropagation()
         e.preventDefault()
         console.log("accept")
     }
+
+
+
     // TODO: convert date to Jan 1 - Dec 31 instead of full dates?
     // TODO: redirect to product page onclick
     // TODO: tags?
@@ -54,7 +86,7 @@ function Listing(props: ListingProps) {
             </div>
             <div className="listing-info">
                 <h4 id="address">{props.data.address}</h4>
-                <p>{props.data.date_start} - {props.data.date_end}</p>
+                <p>Available {props.data.date_start} to {props.data.date_end}</p>
                 <p>{props.data.area} sqft.</p>
                 <p>${props.data.price}/day</p>
             </div>
@@ -62,7 +94,10 @@ function Listing(props: ListingProps) {
             { props.data.user_id !== "" && props.showClaimedBox && // only render claimer and accept/decline for claimed listings
                 <div className="claimed-box">
                     <div className="claiming-user-info">
-                        <h3>Claimed by {props.data.user_id}</h3>
+                        <p>Claimed by</p>
+                        <h4>{userName}</h4>
+                        <p>{userEmail}</p>
+                        <p>{userPhone}</p>
                     </div>
 
                     { props.showAcceptDecline &&
